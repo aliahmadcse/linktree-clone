c2ecd1a2dd5e844a8d9dcf4443f1c4cb0934d444
aliahmadcse <aliahmadcse@gmail.com>
2020-09-03 14:20:37 +0500

    📦 NEW: web routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// linktree-clone.com/dashboard
+Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function () {
+
+    Route::get('/links', 'LinkController@index');
+    Route::get('/links/new', 'LinkController@create');
+    Route::post('links/new', 'LinkController@store');
+    Route::get('/links/{link}', 'LinkController@edit');
+    Route::post('/links/{link}', 'LinkController@update');
+    Route::delete('links/{link}', 'LinkController@destroy');
+
+    Route::get('/settings', 'UserController@edit');
+    Route::post('/settings', 'UserController@update');
+
+    
+});
+
+
+Route::post('visit/{link}', 'VisitController@store');
+
+// linktree-clone.com/username
+Route::get('/{user}', 'UserController@show');
