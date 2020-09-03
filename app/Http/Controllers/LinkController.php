1e10acf24490c3f85bba57a375066de9e09c62f9
aliahmadcse <aliahmadcse@gmail.com>
2020-09-03 19:41:03 +0500

    📦 NEW: edit and delete links

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -40,6 +40,11 @@ class LinkController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|max:255',
+            'link' => 'required|url'
+        ]);
+
         $link = Auth::user()->links()
             ->create($request->only(['name', 'link']));
 
@@ -50,15 +55,62 @@ class LinkController extends Controller
         return redirect()->back();
     }
 
+    /**
+     * Display a view with link to edit
+     *
+     * @param Link $link
+     * @return view
+     */
     public function edit(Link $link)
     {
+        // enforcing that a user can edit only his/her own links
+        if ($link->user_id !== Auth::id()) {
+            return abort(404);
+        }
+        return view('links.edit', [
+            'link' => $link
+        ]);
     }
 
+    /**
+     * Updates a link resource
+     *
+     * @param Request $request
+     * @param Link $link
+     * @return redirect to index action
+     */
     public function update(Request $request, Link $link)
     {
+        // enforcing that a user can edit only his/her own links
+        if ($link->user_id !== Auth::id()) {
+            return abort(403);
+        }
+
+        $request->validate([
+            'name' => 'required|max:255',
+            'link' => 'required|url'
+        ]);
+
+        $link->update($request->only(['name', 'link']));
+
+        return redirect()->to('/dashboard/links');
     }
 
-    public function destory(Request $request, Link $Link)
+
+    /**
+     * Delete a link resource
+     *
+     * @param Request $request
+     * @param Link $Link
+     * @return redirect to index action
+     */
+    public function destroy(Request $request, Link $link)
     {
+        if ($link->user_id !== Auth::id()) {
+            abort(403);
+        }
+        $link->delete();
+
+        return redirect()->to('/dashboard/links');
     }
 }
--- resources/views/links/edit.blade.php
+++ resources/views/links/edit.blade.php
@@ -5,9 +5,9 @@
     <div class="row">
         <div class="col-12 card">
             <div class="card-body">
-                <h2 class="card-title text-center">Create a new Link</h2>
+                <h2 class="card-title text-center">Editing Link</h2>
 
-                <form action="/dashboard/links/new" method="POST">
+                <form action="/dashboard/links/{{ $link->id }}" method="POST">
                     @csrf
                     <div class="row d-flex justify-content-center align-items-center">
 
@@ -16,7 +16,8 @@
                             <div class="form-group">
                                 <label for="name">Link Name</label>
                                 <input id="name" class="form-control {{ $errors->first('name') ? 'is-invalid' : '' }}"
-                                    type="text" name="name" placeholder="My LinkedIn Profile" value="{{ old('name') }}">
+                                    type="text" name="name" placeholder="My LinkedIn Profile"
+                                    value="{{ old('name') ? old('name') : $link->name }}">
 
                                 @if ($errors->first('name'))
                                 <span class="invalid-feedback" role="alert">
@@ -32,7 +33,7 @@
                                 <label for="link">Link URL</label>
                                 <input id="link" class="form-control {{ $errors->first('link') ? 'is-invalid' : '' }}"
                                     type="text" name="link" placeholder="https://www.linkedin.com/in/username/"
-                                    value="{{ old('link') }}">
+                                    value="{{ old('link') ? old('link') : $link->link }}">
 
                                 @if ($errors->first('link'))
                                 <span class="invalid-feedback" role="alert">
@@ -46,6 +47,9 @@
                         <div class="col-lg-8 col-md-6">
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary">Save Link</button>
+                                <button type="button" class="btn btn-danger"
+                                    onclick="event.preventDefault(); document.getElementById('link-delete-form').submit();">Delete
+                                    Link</button>
                             </div>
                         </div>
 
@@ -53,6 +57,11 @@
 
                 </form>
 
+                {{-- delete link form --}}
+                <form id="link-delete-form" action="/dashboard/links/{{ $link->id }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                </form>
             </div>
         </div>
     </div>
