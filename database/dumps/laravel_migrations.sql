f69842285c0d9aaa9b3c12694611728dca91b84f
jay Bender <jayjayb100@msn.com>
2021-06-17 16:29:13 -0500

    removed dumps

--- database/dumps/laravel_migrations.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO laravel.migrations (id, migration, batch) VALUES (1, '2014_10_12_000000_create_users_table', 1);
-INSERT INTO laravel.migrations (id, migration, batch) VALUES (2, '2014_10_12_100000_create_password_resets_table', 1);
-INSERT INTO laravel.migrations (id, migration, batch) VALUES (3, '2019_08_19_000000_create_failed_jobs_table', 1);
-INSERT INTO laravel.migrations (id, migration, batch) VALUES (4, '2020_09_02_182635_create_links_table', 1);
-INSERT INTO laravel.migrations (id, migration, batch) VALUES (5, '2020_09_02_182826_create_visits_table', 1);
